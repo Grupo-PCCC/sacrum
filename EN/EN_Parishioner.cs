b23779c6f685170478632a90418c2c4dc6a2e2ee
Gabriel <43546836+gabrielcovello@users.noreply.github.com>
2020-06-19 20:29:31 -0300

    Agrego las entidades Parishioner, ParishionerPhone y PhoneType

---
+++ EN/EN_Parishioner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace EN
+{
+    public class EN_Parishioner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string NumberP { get; set; }
+        public string NumberA { get; set; }
+        public string Observation { get; set; }
+
+        public EN_ParishionerPhone _parishionerPhone;
+
+        public EN_Parishioner()
+        {
+            _parishionerPhone = new EN_ParishionerPhone();
+        }
+
+    }
+}
---
+++ EN/EN_ParishionerPhone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EN
+{
+    public class EN_ParishionerPhone
+    {
+        public int IdPHP1;
+        public int IdPHP2;
+        public string NumberP { get; set; }
+        public string NumberA { get; set; }
+        public EN_PhoneType _phoneType;
+
+
+        public EN_ParishionerPhone()
+        {
+            _phoneType = new EN_PhoneType();
+        }
+
+    }
+}
