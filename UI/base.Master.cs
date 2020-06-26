4d644d8b4754ed1523b60159e2066fddcbf38fe2
Hernan Pereira <hernan.ariel.pereira@gmail.com>
2020-06-26 01:21:40 -0300

    Creacion de PageMaster

---
+++ UI/base.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UI
+{
+    public partial class _base : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (Session["usuarioId"] == null)
+            //{
+            //    Response.Redirect("~/Inicio.aspx");
+            //}
+        }
+
+        protected void lnkCerrarSesion_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Response.Redirect("~/Inicio.aspx");
+        }
+    }
+};
