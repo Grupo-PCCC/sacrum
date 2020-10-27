22a142d16575764c64f47812cad01409a664d99c
Gabriel <43546836+gabrielcovello@users.noreply.github.com>
2020-10-27 00:45:57 -0300

    Inicia módulo proveedores

---
+++ EN/EN_Proveedor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace EN
+{
+    public class EN_Proveedor
+    {
+
+        public int Id { get; set; }
+        public string CodigoInterno { get; set; }
+        public string RazonSocial { get; set; }
+        public string Telefono { get; set; }
+        public int IdTelefono { get; set; }
+        public string Mail { get; set; }
+        public int IdMail { get; set; }
+        public string Direccion { get; set; }
+        public int IdDireccion { get; set; }
+        public string TDoc { get; set; }
+        public string Observaciones { get; set; }
+        public string Tabla { get; set; }
+        public int IdEntidad { get; set; }
+        public int IdTipoEntidad { get; set; }
+        public int Estado { get; set; }
+
+        public EN_TipoDocumento _TipoDocumento;
+        public EN_Entidad _Entidad;
+        public EN_DatoEntidad _DatoEntidad;
+        public EN_Proveedor()
+        {
+            _TipoDocumento = new EN_TipoDocumento();
+            _Entidad = new EN_Entidad();
+            _DatoEntidad = new EN_DatoEntidad();
+
+        }
+
+
+    }
+
+
+}
--- database/sacrumdb-tests.sql
+++ database/sacrumdb-tests.sql
@@ -1,139 +1,178 @@
-USE master
-GO
-CREATE DATABASE SacrumDb_tests
-GO
-USE SacrumDb_tests
-GO
-CREATE TABLE TipoEntidad (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Nombre VARCHAR (9) NOT NULL)
-GO
-INSERT INTO TipoEntidad VALUES ('Feligres'), ('Proveedor'), ('Usuario'), ('Fallecido')
-GO
-CREATE TABLE Entidad (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-IdTipoEntidad INT FOREIGN KEY REFERENCES TipoEntidad(Id),
-CodigoInterno VARCHAR (8),
-Estado INT NOT NULL)
-GO
-CREATE TABLE TipoDocumento (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Nombre VARCHAR (3)
-)
-GO
-INSERT INTO TipoDocumento VALUES ('DNI'), ('LC'), ('LE'), ('PAS')
-GO
-CREATE TABLE Feligres (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-CodigoInterno VARCHAR (8),
-Nombre VARCHAR (30) NOT NULL,
-Apellido VARCHAR (30) NOT NULL,
-FechaNacimiento DATE,
-Documento VARCHAR (20) NOT NULL,
-IdTipoDocumento INT FOREIGN KEY REFERENCES TipoDocumento(Id) NOT NULL,
-Observaciones VARCHAR (120),
-Vivo INT NOT NULL,--Si el fallecido es un feligrÃ©s, se cargan los datos automÃ¡ticamente y cambia: Vivo=0. Copia un nuevo fallecido con los mismos datos.
-IdEntidad INT FOREIGN KEY REFERENCES Entidad(Id),
-EsContacto INT NOT NULL)
-GO
-CREATE TABLE Proveedor (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-CodigoInterno VARCHAR (8),
-RazonSocial VARCHAR (50) NOT NULL,
-Observaciones VARCHAR (120),
-IdEntidad INT FOREIGN KEY REFERENCES Entidad(Id))
-GO
-CREATE TABLE TipoUsuario (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Nombre VARCHAR (14) NOT NULL)
-GO
-INSERT INTO TipoUsuario VALUES ('Administrador'),('Operador')
-GO
-CREATE TABLE Usuario (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-CodigoInterno VARCHAR (8),
-Nombre VARCHAR (30) NOT NULL,
-Apellido VARCHAR (30) NOT NULL,
-Nick VARCHAR (15),
-Contrasenia VARCHAR (255),
-IdTipoUsuario INT FOREIGN KEY REFERENCES TipoUsuario(Id) NOT NULL,
-IdEntidad INT FOREIGN KEY REFERENCES Entidad(Id))
-GO
-CREATE TABLE TipoCategoria
-(
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Nombre VARCHAR (7) NOT NULL,
-)
-GO
-INSERT INTO TipoCategoria VALUES ('Ingreso'), ('Egreso')
-GO
-CREATE TABLE Categoria
-(
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Nombre VARCHAR (30) NOT NULL,
-IdTipoCategoria INT FOREIGN KEY REFERENCES TipoCategoria(Id) NOT NULL
-)
-GO
-CREATE TABLE MovimientoMonetario
-(
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Valor FLOAT NOT NULL,
-Fecha DATE NOT NULL,
-Observacion VARCHAR (120),
-IdUsuario INT FOREIGN KEY REFERENCES Usuario(Id) NOT NULL,
-IdCategoria INT FOREIGN KEY REFERENCES Categoria(Id) NOT NULL
-)
-CREATE TABLE Fallecido (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-CodigoInterno VARCHAR (8),
-Nombre VARCHAR (30) NOT NULL,
-Apellido VARCHAR (30) NOT NULL,
-Documento VARCHAR (20) NOT NULL,
-FechaFallecimiento DATE NOT NULL,
-FechaIngresoCinerario DATE NOT NULL,
-Observaciones VARCHAR (120),
-Contribuyo INT NOT NULL,
-IdMovimientoMonetario INT FOREIGN KEY REFERENCES MovimientoMonetario(Id),
-IdTipoDocumento INT FOREIGN KEY REFERENCES TipoDocumento(Id) NOT NULL,
-IdEntidad INT FOREIGN KEY REFERENCES Entidad(Id))
-GO
-CREATE TABLE ContactoFallecido (
-IdFeligres INT FOREIGN KEY REFERENCES Feligres(Id) NOT NULL,
-IdFallecido INT FOREIGN KEY REFERENCES Fallecido(Id) NOT NULL
-)
-GO
-CREATE TABLE TipoDatoEntidad (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Nombre VARCHAR (9) NOT NULL)
-GO
-INSERT INTO TipoDatoEntidad VALUES ('Principal'), ('Otro')
-GO
-CREATE TABLE DatoEntidad (
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-IdEntidad INT FOREIGN KEY REFERENCES Entidad(Id) NOT NULL,
-IdTipoDatoEntidad INT FOREIGN KEY REFERENCES TipoDatoEntidad(Id) NOT NULL,
-NombreDato VARCHAR (4) NOT NULL, --(tel,mail,dire. Lo mandamos desde la APP)
-Valor VARCHAR (100) NOT NULL,
-Detalle VARCHAR (20))
-GO
-CREATE TABLE Actividad
-(
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Nombre VARCHAR (50) NOT NULL,
-Observacion VARCHAR (120)
-)
-GO
-CREATE TABLE FeligresesPorActividad
-(
-IdFeligres INT FOREIGN KEY REFERENCES Feligres(Id) NOT NULL,
-IdActividad INT FOREIGN KEY REFERENCES Actividad(Id) NOT NULL,
-)
-GO
-CREATE TABLE Auditoria
-(
-Id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
-Fecha DATETIME NOT NULL,
-Accion VARCHAR (240) NOT NULL,
-IdUsuario INT FOREIGN KEY REFERENCES Usuario(Id) NOT NULL
-)
-GO
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Proveedores.aspx.cs" MasterPageFile="/base.Master" Inherits="UI.Parishioner" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server"></asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%--</form>--%>
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Proveedores</h1>
+                        <div class="row">
+                            <div class="col-md-2 ml-0">
+                                <asp:Button ID="btnNuevo" CssClass="btn btn-primary" runat="server" Text="Nuevo proveedor" OnClick="btnNuevo_Click" />
+                            </div>
+                        
+                            <div class="col-md-10 ml-0">
+							    <!-- Filtro -->
+							    <div class="panel">
+								    <div class="panel-heading">
+									    <h3 class="panel-title">Filtro</h3>
+									    <div class="right">
+										    <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+									    </div>
+								    </div>
+								    <div class="panel-body">
+									    <div class="form-inline mx-sm-3 mb-2">
+                                            <asp:TextBox ID="txtRazonSocialBuscar" class="form-control mr-5" PlaceHolder="Razón social" runat="server"></asp:TextBox>
+                                            <asp:Button ID="Button1" runat="server" class="btn btn-secondary" OnClick="BtnBuscar_Click" Text="Buscar" />
+                                            </div>
+                                        </div>
+								    </div>
+							    </div>
+							    <!-- Filtro -->
+						    </div>
+                        </div>
+                        
+
+                        <asp:GridView ID="dgvProveedor" runat="server" class="table table-striped text-center" CellPadding="4" OnRowCommand="ViewProveedor_RowCommand1" AutoGenerateColumns="False" DataKeyNames="IdEntidad" CellSpacing="2"  AllowPaging="true" OnPageIndexChanging="dgvProveedor_PageIndexChanging" OnSelectedIndexChanged="dgvProveedor_SelectedIndexChanged">
+                            <Columns>
+                                <asp:BoundField HeaderText="Id" DataField="Id" Visible="False">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+                                <asp:BoundField HeaderText="Codigo Interno" DataField="CodigoInterno">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+                                <asp:BoundField HeaderText="Razón social" DataField="RazonSocial">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+                                
+                                <asp:BoundField HeaderText="Telefono" DataField="Telefono">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+
+                                <asp:BoundField HeaderText="Mail" DataField="Mail">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+
+                                <asp:BoundField HeaderText="Direccion" DataField="Direccion">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+
+                                <asp:BoundField HeaderText="Observaciones" DataField="Observaciones" Visible="False">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+
+                                <asp:BoundField HeaderText="IdEntidad" DataField="IdEntidad" Visible="False">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:BoundField>
+
+                                <asp:ButtonField CommandName="Seleccionar" Text="Editar" HeaderText="Editar">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:ButtonField>
+                                <asp:ButtonField CommandName="Borrar" Text="Borrar" HeaderText="Borrar">
+                                    <HeaderStyle HorizontalAlign="Center" />
+                                    <ItemStyle HorizontalAlign="Center" />
+                                </asp:ButtonField>
+                            </Columns>
+                            <%--<FooterStyle BackColor="#CCCCCC" />
+                            <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" HorizontalAlign="Center" />
+                            <PagerStyle BackColor="#CCCCCC" ForeColor="Black" HorizontalAlign="Left" />
+                            <RowStyle BackColor="White" />
+                            <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+                            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                            <SortedAscendingHeaderStyle BackColor="#808080" />
+                            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                            <SortedDescendingHeaderStyle BackColor="#383838" />--%>
+                        </asp:GridView>
+
+
+                        <br />
+                        <asp:Label ID="lblResultado" runat="server" Text=""></asp:Label>
+                    </div>
+                </div>
+            </div>
+
+            <asp:HiddenField ID="hid" runat="server" />
+
+
+
+            <asp:Panel ID="pnlProveedor" runat="server" CssClass="modalpopup">
+
+                <div class="modal-header" id="ModalHeader">
+
+                    <h3>Proveedor</h3>
+                </div>
+                <div class="modal-body">
+                    <div class="form form-inline">
+                        <div class="form form-group">
+                            <label for="RazonSocial" runat="server">Razón social</label>
+                            <asp:TextBox ID="txtNombre" class="form-control" runat="server" Width="350px"></asp:TextBox>
+                        </div>
+                    <div class="form form-inline">
+                        <div class="form form-group">
+                            <label for="direccion" runat="server">Dirección</label>
+                            <asp:TextBox ID="txtDireccion" class="form-control" runat="server" Width="350px"></asp:TextBox>
+                        </div>
+
+                        <div class="form form-group">
+                            <label for="mail" runat="server">E-Mail</label>
+                            <asp:TextBox ID="txtMail" class="form-control" runat="server" Width="350px"></asp:TextBox>
+                        </div>
+                    </div>
+                    <div class="form form-inline">
+                        <div class="form form-group">
+                            <label for="TTel" runat="server">Tipo de Telefono</label>
+                            <asp:DropDownList ID="lstTelefono" runat="server" class="form-control">
+                                <asp:ListItem Value="1">Principal</asp:ListItem>
+                                <asp:ListItem Value="2">Otro</asp:ListItem>
+                            </asp:DropDownList>
+                        </div>
+                        <div class="form form-group">
+                            <label for="Telefono" runat="server">Telefono</label>
+                            <asp:TextBox ID="txtTelefono" class="form-control" runat="server" Width="350px"></asp:TextBox>
+                        </div>
+                    </div>
+
+                    <div class="form form-group">
+                        <label for="observaciones" runat="server">Observaciones</label>
+                        <asp:TextBox ID="txtObservaciones" class="form-control" runat="server" Width="350px"></asp:TextBox>
+                    </div>
+                    <br />
+                     <div class="form form-group">
+                        <asp:Button ID="btnGrabar" CssClass="btn-success" runat="server" Text="Guardar" OnClick="btnGrabar_Click" />
+                        <asp:Button ID="btnCancelar" CssClass="btn-danger" runat="server" Text="Cancelar" OnClick="btnCancelar_Click" />
+                    </div>
+                </div>
+            </asp:Panel>
+
+            <ajaxToolkit:ModalPopupExtender ID="ModalPopupExtender1" runat="server" BackgroundCssClass="modalBackground" PopupControlID="pnlProveedor" TargetControlID="btnNuevo" BehaviorID="modalPopupExtender1">
+            </ajaxToolkit:ModalPopupExtender>
+
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+
+
+    <br />
+    <br />
+    <br />
+
+
+    <%--</form>--%>
+</asp:Content>
